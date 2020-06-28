d148505e8f53521504b15055e02e16ac7a6e1095
obliviousgarden <tokisakiarashi@gmail.com>
2020-06-28 16:03:19 +0900

    第4次提交 神经网络练习 rnn连续神经网络

--- app/tensorflow/test/rnn.py
+++ app/tensorflow/test/rnn.py
@@ -1,7 +1,41 @@
 import tensorflow as tf
 import numpy as np
 
+
+# 循环神经网络：广泛用于语言模型、文本生成、机器翻译等
+# RNN原理: 每次迭代如下
+# 1 W线性变换矩阵[n×n] * 上一个状态s(t-1)[n] + U线性变换矩阵[m×n] * 当前输入x(t)[m] => 当前状态s(t)[n]
+# 2 V线性变换矩阵[n×p] * 当前状态s(t)[n] => 当前输出o(t)[p]
+# 3 当前状态s(t)进入下一个迭代
 # 1 数据
+class RNN(tf.keras.Model):
+    def __init__(self, num_chars, batch_size, seq_length):
+        super().__init__()
+        self.num_chars = num_chars
+        self.seq_length = seq_length
+        self.batch_size = batch_size
+        self.cell = tf.keras.layers.LSTMCell(units=256)
+        self.dense = tf.keras.layers.Dense(units=self.num_chars)
+
+    def call(self, inputs, from_logits=False):
+        inputs = tf.one_hot(inputs, depth=self.num_chars)       # [batch_size, seq_length, num_chars]
+        state = self.cell.get_initial_state(batch_size=self.batch_size, dtype=tf.float32)
+        for t in range(self.seq_length):
+            output, state = self.cell(inputs[:, t, :], state)
+        logits = self.dense(output)
+        if from_logits:
+            return logits
+        else:
+            return tf.nn.softmax(logits)
+
+    def predict(self, inputs, temperature=1.):
+        batch_size, _ = tf.shape(inputs)
+        logits = self(inputs, from_logits=True)
+        prob = tf.nn.softmax(logits / temperature).numpy()
+        return np.array([np.random.choice(self.num_chars, p=prob[i, :])
+                         for i in range(batch_size.numpy())])
+
+
 class DataLoader():
     def __init__(self):
         path = tf.keras.utils.get_file('nietzsche.txt',
@@ -21,3 +55,33 @@ class DataLoader():
             seq.append(self.text[index:index+seq_length])
             next_char.append(self.text[index+seq_length])
         return np.array(seq), np.array(next_char)       # [batch_size, seq_length], [num_batch]
+
+
+if __name__ == '__main__':
+    num_batches = 1000
+    seq_length = 40
+    batch_size = 50
+    learning_rate = 1e-3
+
+    data_loader = DataLoader()
+    model = RNN(num_chars=len(data_loader.chars), batch_size=batch_size, seq_length=seq_length)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    for batch_index in range(num_batches):
+        X, y = data_loader.get_batch(seq_length, batch_size)
+        with tf.GradientTape() as tape:
+            y_pred = model(X)
+            loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+            loss = tf.reduce_mean(loss)
+            print("batch %d: loss %f" % (batch_index, loss.numpy()))
+        grads = tape.gradient(loss, model.variables)
+        optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+
+    X_, _ = data_loader.get_batch(seq_length, 1)
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        X = X_
+        print("diversity %f:" % diversity)
+        for t in range(400):
+            y_pred = model.predict(X, diversity)
+            print(data_loader.indices_char[y_pred[0]], end='', flush=True)
+            X = np.concatenate([X[:, 1:], np.expand_dims(y_pred, axis=1)], axis=-1)
+        print("\n")
