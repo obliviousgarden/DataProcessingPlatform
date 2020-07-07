9994191d9eb1f5a8f768b5eac51c0aee83b61f7d
obliviousgarden <tokisakiarashi@gmail.com>
2020-07-07 23:34:52 +0900

    第9次提交 放弃多级神经网络 换成多神经网络的组合

--- app/tensorflow/biasdrivendielectric.py
+++ app/tensorflow/biasdrivendielectric.py
@@ -67,6 +67,15 @@ def func(omega, epsilon_inf, delta_epsilon, tau):
     return tf.square(tf.multiply(omega, tau))
 
 
+def main2(dc_bias):
+
+    data_loader = DataLoader()
+    freq_test, epsilon_test = np.array(data_loader.get_data(dc_bias), dtype='float32')
+
+    alpha = 0.9
+    beta=0.8
+    rate = 0.0001
+
 def main(dc_bias):
     # START:利用现行回归对colecole拟合成功，暂时封上
     # data_loader = DataLoader()
@@ -116,134 +125,91 @@ def main(dc_bias):
     freq_test = freq_test[:, np.newaxis]
     omega_test = np.multiply(freq_test, 2.0 * np.pi)
     epsilon_test = epsilon_test[:, np.newaxis]
+    alpha_target = 0.95
+    beta_target = 0.85
     print('reshape维数到[None,1],频率数据集:{},介电数据集:{}'.format(freq_test.shape, epsilon_test.shape))
     fig = plt.figure()
     plt.ion()
     plt.show()
-    ax_331 = fig.add_subplot(3, 3, 1)
-    ax_331.set_xscale('log')
-    ax_331.scatter(freq_test, epsilon_test, label='epsilon(freq)')
-    ax_331.legend()
+    ax_531 = fig.add_subplot(5, 3, 1)
+    ax_531.set_xscale('log')
+    ax_531.scatter(freq_test, epsilon_test, label='epsilon(freq)')
+    ax_531.legend()
 
     x_target = np.log(omega_test + np.random.normal(0.0, 10.0, omega_test.shape))
-    W1_target = 0.9
-    b1_target = 0.9 * np.log(0.000001)
-    y_apos_apos_target = np.multiply(x_target, W1_target) + b1_target + + np.random.normal(0.0, 0.1, x_target.shape)
-    ax_332 = fig.add_subplot(3, 3, 2)
-    ax_332.set_xscale('log')
-    ax_332.scatter(omega_test, y_apos_apos_target, label='y\'\'(omega)_T')
-    ax_332.legend()
-    # FIXME:需要记录一下极值帮助f_apos在输入时的归一化
-    f_apos_target = np.exp(y_apos_apos_target) + np.random.normal(0.0, 0.1, y_apos_apos_target.shape)
-    f_apos_target_max = np.max(f_apos_target)
-    f_apos_target_min = np.min(f_apos_target)
-    f_apos_target_mean= np.mean(f_apos_target)
-    f_apos_target_std = np.std(f_apos_target)
-    y_apos_target = 1.0 + np.square(f_apos_target) - 2.0 * f_apos_target * np.cos(0.9 * np.pi / 2.0) + np.random.normal(
-        0.0, 0.1, f_apos_target.shape)
-    ax_333 = fig.add_subplot(3, 3, 3)
-    ax_333.scatter(omega_test, y_apos_target, label='y\'(omega)_T')
-    ax_333.legend()
+    W1_target = alpha_target
+    b1_target = alpha_target * np.log(0.000001)
+    y_apos_apos_apos_target = np.multiply(x_target, W1_target) + b1_target + + np.random.normal(0.0, 0.1, x_target.shape)
+    ax_534 = fig.add_subplot(5, 3, 4)
+    ax_534.set_xscale('log')
+    ax_534.scatter(omega_test, y_apos_apos_apos_target, label='y\'\'\'(omega)_T')
+    ax_534.legend()
 
-    # print(np.any(np.isnan(y_apos_target)))
-
+    # 下面是BP神经网络
     tf.compat.v1.disable_eager_execution()
     omega = tf.compat.v1.placeholder(tf.float32, [None, 1], name='INPUT_omega')
     epsilon = tf.compat.v1.placeholder(tf.float32, [None, 1], name='OUTPUT_epsilon')  # 计算途中它会代表各种各样的量
 
-    # 层1 omega(150,1)->x(150,1)
+    # 第一个线性变换的BP网络
+    # omega(150,1)->x(150,1)
     x = tf.math.log(omega)
-    # 层2 x(150,1)->y''(150,16)
-    W1 = tf.Variable(tf.compat.v1.truncated_normal([1, 16], mean=0.9, stddev=0.06, dtype='float32', name='W1_alpha'))
+    # x(150,1)->y'''(150,16)
+    W1 = tf.Variable(tf.compat.v1.truncated_normal([1, 64], mean=0.95, stddev=0.03, dtype='float32', name='W1_alpha'))
     b1 = tf.Variable(
-        tf.compat.v1.truncated_normal([1, 16], mean=-15.0, stddev=10.0, dtype='float32', name='b1_alpha_LN_tau'))
+        tf.compat.v1.truncated_normal([1, 64], mean=-14.0, stddev=5.0, dtype='float32', name='b1_alpha_LN_tau'))
     Wx_plus_b1 = tf.matmul(x, W1) + b1  # (150,16)
-    y_apos_apos = Wx_plus_b1  # 线性激活函数
+    y_apos_apos_apos = Wx_plus_b1 # 线性激活函数
 
-    # START:输出层(第一次线性规划使用)
-    W1_out = tf.Variable(tf.divide(tf.ones([16, 1]), 16))
+    W1_out = tf.Variable(tf.divide(tf.ones([64, 1]), 64))
     b1_out = tf.Variable(tf.zeros([1, 1]))
-    Wx_plus_b1_out = tf.matmul(y_apos_apos, W1_out) + b1_out
+    Wx_plus_b1_out = tf.matmul(y_apos_apos_apos, W1_out) + b1_out
     output_1 = Wx_plus_b1_out
-    # END
 
-    # 层3 y''(150,16)->f'(150,16)
-    # FIXME:这里可能需要归一化，防止loss下降太慢
-    # 普通归一化或者0均值归一化
-    f_apos = tf.math.exp(y_apos_apos)
-    # TODO：0均值归一化
+    loss = tf.reduce_mean(tf.square(y_apos_apos_apos_target - output_1))
+    # loss = tf.nn.softmax_cross_entropy_with_logits(labels=output_1, logits=y_apos_apos_apos_target)
 
-    # 层4 f'(150,16)->y'(150,64)
-    W21 = tf.Variable(
-        tf.divide(tf.ones([16, 16]), 1), name='W21_1')
-    W22 = tf.Variable(
-        tf.compat.v1.truncated_normal([16, 16], mean=-1.8, stddev=0.11, dtype='float32', name='W22_-2_COS_alpha_PI_/2'))
-    b2 = tf.Variable(tf.ones([1, 16], name='b2_1'))
-    # Wx_plus_b2 = tf.matmul(tf.square(f_apos), W21) + tf.matmul(f_apos, W22) + b2
-    # 归一化会导致这里的平方值出现不预期的误差
-    Wx_plus_b2 = tf.matmul(tf.square(f_apos), W21) + tf.matmul(f_apos, W22) + b2
-    y_apos = tf.tanh(Wx_plus_b2)
-    # y_apos = tf.nn.sigmoid(Wx_plus_b2)
-
-    # START:输出层(第二次二次规划规划使用)
-    W2_out = tf.Variable(tf.divide(tf.ones([16, 1]), 16))
-    b2_out = tf.Variable(tf.ones([1, 1]))
-    Wx_plus_b2_out = tf.matmul(y_apos, W2_out) + b2_out
-    output_2 = Wx_plus_b2_out
-    # END
-
-    # loss = tf.reduce_mean(tf.square(y_apos_apos_target - output_1))
-    loss = tf.reduce_mean(tf.square(y_apos_target - output_2))
-    optimizer = tf.compat.v1.train.GradientDescentOptimizer(0.00001).minimize(loss)
+    optimizer = tf.compat.v1.train.GradientDescentOptimizer(0.0001).minimize(loss)
     init = tf.compat.v1.global_variables_initializer()
     with tf.compat.v1.Session() as sess:
-        # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+        # sess = tf_debug.LocalCLIDebugWrapperSession(sess,ui_type="readline")
         # sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
         sess.run(init)
-        for _ in range(10000):
+        for _ in range(5000):
             # ！！！注意此处是epsilon不是output
-            # sess.run(optimizer, feed_dict={omega: omega_test, epsilon: y_apos_apos_target})
-            sess.run(optimizer, feed_dict={omega: omega_test, epsilon: y_apos_target})
-            if _ % 50 == 0:
-                # print(sess.run(loss, feed_dict={omega: omega_test, epsilon: y_apos_apos_target}))
-                print(sess.run(loss, feed_dict={omega: omega_test, epsilon: y_apos_target}))
-        # 获得预测值
-        # y_apos_apos_pred = sess.run(output_1, feed_dict={omega: omega_test})
-        y_apos_pred = sess.run(output_2, feed_dict={omega: omega_test})
-    # ax_332.plot(omega_test, y_apos_apos_pred, 'r-', label='y\'\'(omega)_P')
-    # ax_332.legend()
-    ax_333.plot(omega_test, y_apos_pred, 'r-', label='y\'(omega)_P')
-    ax_333.legend()
+            sess.run(optimizer, feed_dict={omega: omega_test, epsilon: y_apos_apos_apos_target})
+            if _ % 100 == 0:
+                print('loss',sess.run(loss, feed_dict={omega: omega_test, epsilon: y_apos_apos_apos_target}))
+        # START 预测值
+        y_apos_apos_apos_pred = sess.run(output_1, feed_dict={omega: omega_test})
+        W1_pred = sess.run(W1, feed_dict={omega: omega_test})
+        b1_pred = sess.run(b1, feed_dict={omega: omega_test})
+        W1_pred_mean = tf.reduce_mean(W1_pred).eval()
+        b1_pred_mean = tf.reduce_mean(b1_pred).eval()
+        # END
+    ax_534.plot(omega_test, y_apos_apos_apos_pred, 'r-', label='y\'\'\'(omega)')
+    ax_535 = fig.add_subplot(5, 3, 5)
+    ax_536 = fig.add_subplot(5, 3, 6)
+    ax_535.hist(W1_pred[0], rwidth=0.9, label='W1_pred_mean='+str(W1_pred_mean))
+    ax_536.hist(b1_pred[0], rwidth=0.9, label='b1_pred_mean='+str(b1_pred_mean))
+    ax_534.legend()
+    ax_535.legend()
+    ax_536.legend()
 
-    # # 中间层
-    # W1 = tf.Variable(tf.compat.v1.random_normal([1, 10]))
-    # b1 = tf.Variable(tf.compat.v1.zeros([1, 10]))
-    # Wx_plus_b1 = tf.matmul(x, W1) + b1
-    # l1 = tf.nn.tanh(Wx_plus_b1)
-    # # 输出层
-    # W2 = tf.Variable(tf.compat.v1.random_normal([10, 1]))
-    # b2 = tf.Variable(tf.compat.v1.zeros([1, 1]))
-    # Wx_plus_b2 = tf.matmul(l1, W2) + b2
-    # pred = tf.nn.tanh(Wx_plus_b2)
-    # # 定义loss函数
-    # loss = tf.reduce_mean(tf.square(y - pred))
-    # cross_entropy =  tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=pred))
-    # optimizer = tf.compat.v1.train.GradientDescentOptimizer(0.000000001).minimize(loss)
-    # # 变量初始化
-    # init = tf.compat.v1.global_variables_initializer()
-    #
-    # with tf.compat.v1.Session() as sess:
-    #     sess.run(init)
-    #     for _ in range(3000):
-    #         sess.run(optimizer, feed_dict={x: freq_test, y: y_test})
-    #     # 获得预测值
-    #     pred_value = sess.run(pred, feed_dict={x: freq_test})
-    #
-    # plt.figure()
-    # plt.scatter(freq_test, y_test)
-    # plt.plot(freq_test, pred_value, 'r-', lw=3)
-    # plt.show()
-    # END
+    # 第二个线性变换的BP网络
+    f_apos_apos_target = np.exp(y_apos_apos_apos_target)
+    y_apos_apos_target = np.abs(f_apos_apos_target-np.cos(alpha_target*np.pi/2.0))
+    ax_537 = fig.add_subplot(5, 3, 7)
+    ax_537.scatter(omega_test, y_apos_apos_target, label='y\'\'(omega)_T')
+    ax_537.legend()
+
+
+
+
+
+
+
+
+
 
     # Keras 不是很好用 结果都是常数
     # data_loader = DataLoader()
@@ -288,3 +254,4 @@ def main(dc_bias):
 
 if __name__ == '__main__':
     main(0.0)
+    # main2(0.0)
