878265cc4fdf50d6715b8cace80fc72ef43afd89
obliviousgarden <tokisakiarashi@gmail.com>
2020-07-10 23:47:59 +0900

    第13次提交 v1.0alpha版本完成 后续还有需求：导出csv文件，参数的上下限设定，代码还有很多冗余的地方（尤其是对model参数的处理），另外有一些东西可以单独封入一个py文件内，一些公共方法可以放到一个工具py内

--- app/ui/simulator.py
+++ app/ui/simulator.py
@@ -1,7 +1,15 @@
+import matplotlib
 from PyQt5 import QtCore, QtGui, QtWidgets
 from simulator_ui import Ui_MainWindow
 import numpy as np
 import os, sys
+import matplotlib.pyplot as plt
+import random
+
+matplotlib.use("Qt5Agg")  # 声明使用QT5
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from dielectricrelaxationsimulator import DielectricRelaxationSimulator, func_Havriliak_Negami, \
+    func_Cole_Cole, func_Cole_Davidson, func_Debye
 
 
 class Simulator(Ui_MainWindow):
@@ -31,6 +39,14 @@ class Simulator(Ui_MainWindow):
         self.file_name = []
         self.list_view_file_model = QtCore.QStringListModel()
         self.list_view_file_model.setStringList(self.file_name)
+        self.list_view_results_model = QtGui.QStandardItemModel()
+        # file_name为键
+        self.result_dict = {}
+        # plot相关
+        self.dialog_plot = QtWidgets.QDialog()
+        self.layout_plot = QtWidgets.QVBoxLayout()
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
 
     def setupUi(self, MainWindow):
         # 继承调用父类的
@@ -57,18 +73,28 @@ class Simulator(Ui_MainWindow):
         # 设定PushButton
         self.PushButton_file.clicked.connect(self.on_PushButton_file_clicked)
         self.PushButton_dir.clicked.connect(self.on_PushButton_dir_clicked)
-
         self.PushButton_simulate.clicked.connect(self.on_PushButton_simulate_clicked)
+        self.PushButton_plot.clicked.connect(self.on_PushButton_plot_clicked)
         # 设定ListView内的Model
         self.ListView_file.setModel(self.list_view_file_model)
+        self.ListView_results.setModel(self.list_view_results_model)
         # 禁止编辑
         self.ListView_file.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.ListView_results.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         # 设定Slider
         self.Slider_alpha.valueChanged.connect(self.on_Slider_alpha_valueChanged)
         self.Slider_beta.valueChanged.connect(self.on_Slider_beta_valueChanged)
         self.Slider_tau.valueChanged.connect(self.on_Slider_tau_valueChanged)
         self.Slider_epsiloninf.valueChanged.connect(self.on_Slider_epsiloninf_valueChanged)
         self.Slider_deltaepsilon.valueChanged.connect(self.on_Slider_deltaepsilon_valueChanged)
+        # 初始化figure和cavas
+        self.dialog_plot.setWindowTitle('Plot results')
+        self.layout_plot.addWidget(self.canvas)
+        self.dialog_plot.setLayout(self.layout_plot)
+        # 菜单功能
+        self.actionQuit.triggered.connect(self.on_Action_quit)
+        self.actionSaveResultsAs.triggered.connect(self.on_Action_save_results_as)
+        self.actionParameters_Range.triggered.connect(self.on_Action_parameters_range)
 
     def on_RadioButton_clicked(self):
         if self.RadioButton_hnmodel.isChecked():
@@ -122,24 +148,150 @@ class Simulator(Ui_MainWindow):
 
     def on_Slider_tau_valueChanged(self):
         self.tau = np.power(10, np.log10(self.tau_min) + (
-                    np.log10(self.tau_max) - np.log10(self.tau_min)) * self.Slider_tau.value() / 100.0)
+                np.log10(self.tau_max) - np.log10(self.tau_min)) * self.Slider_tau.value() / 100.0)
         self.lcdNumber_tau.display('%.1e' % self.tau)
 
     def on_Slider_epsiloninf_valueChanged(self):
         self.epsiloninf = self.epsiloninf_min + (
-                    self.epsiloninf_max - self.epsiloninf_min) * self.Slider_epsiloninf.value() / 100.0
+                self.epsiloninf_max - self.epsiloninf_min) * self.Slider_epsiloninf.value() / 100.0
         self.lcdNumber_epsiloninf.display(str(self.epsiloninf))
 
     def on_Slider_deltaepsilon_valueChanged(self):
         self.deltaepsilon = self.deltaepsilon_min + (
-                    self.deltaepsilon_max - self.deltaepsilon_min) * self.Slider_deltaepsilon.value() / 100.0
+                self.deltaepsilon_max - self.deltaepsilon_min) * self.Slider_deltaepsilon.value() / 100.0
         self.lcdNumber_deltaepsilon.display(str(self.deltaepsilon))
 
     def on_PushButton_simulate_clicked(self):
-        print('SIMULATE!!!')
-        print(self.file_path)
-        print(self.alpha, self.beta, self.tau, self.epsiloninf, self.deltaepsilon)
-        # TODO:传输文件数据调取模拟模块
+        # 这里需要根据model构造p0和bounds参数
+        p0 = []
+        bounds = ([], [])
+        if self.model == 1:
+            p0.append(self.alpha)
+            p0.append(self.beta)
+            bounds[0].append(self.alpha_min)
+            bounds[0].append(self.beta_min)
+            bounds[1].append(self.alpha_max)
+            bounds[1].append(self.beta_max)
+        elif self.model == 2:
+            p0.append(self.alpha)
+            bounds[0].append(self.alpha_min)
+            bounds[1].append(self.alpha_max)
+        elif self.model == 3:
+            p0.append(self.beta)
+            bounds[0].append(self.beta_min)
+            bounds[1].append(self.beta_max)
+        else:
+            pass
+        p0.extend([self.tau, self.epsiloninf, self.deltaepsilon])
+        bounds[0].extend([self.tau_min, self.epsiloninf_min, self.deltaepsilon_min])
+        bounds[1].extend([self.tau_max, self.epsiloninf_max, self.deltaepsilon_max])
+        print('初始值', p0)
+        print('边界', bounds)
+        self.list_view_results_model.clear()
+        for i in range(self.file_path.__len__()):
+            print(i, self.file_name[i])
+            self.result_dict[self.file_name[i]] = DielectricRelaxationSimulator(self.model, self.file_path[i],
+                                                                                p0=p0,
+                                                                                bounds=bounds).simulate()
+            result = self.result_dict[self.file_name[i]]['popt']
+
+            item = QtGui.QStandardItem(" #{}\nAlpha={},Beta={}Tau={}\nEpsilon_Inf={}Delta_Epsilon={}"
+                                       .format(self.file_name[i]
+                                               , {1: result[0], 2: result[0], 3: 1.0, 4: 1.0}[self.model]
+                                               , {1: result[1], 2: 1.0, 3: result[0], 4: 1.0}[self.model]
+                                               , result[-3], result[-2], result[-1]))
+            item.setCheckable(True)
+            self.list_view_results_model.appendRow(item)
+        print('结果', self.result_dict)
+
+    def on_PushButton_plot_clicked(self):
+        plot_data_dict = {}
+        for i in range(self.list_view_results_model.rowCount()):
+            item = self.list_view_results_model.item(i)
+            if item.isCheckable() and item.checkState() == QtCore.Qt.Checked:
+                plot_data_dict[item.text().split('\n')[0].replace(' #', '')] = {}
+        for i in range(self.file_name.__len__()):
+            if self.file_name[i] in plot_data_dict.keys():
+                freq_list, epsilon_raw_list = DielectricRelaxationSimulator(self.model, self.file_path[i]).get_data()
+                plot_data = plot_data_dict[self.file_name[i]]
+                param_list = self.result_dict[self.file_name[i]]['popt']
+                plot_data['freq'] = freq_list
+                plot_data['epsilon_raw'] = epsilon_raw_list
+                if self.model == 1:
+                    plot_data['epsilon'] = func_Havriliak_Negami(freq_list, alpha=param_list[0], beta=param_list[1],
+                                                                 tau=param_list[2], epsilon_inf=param_list[3],
+                                                                 delta_epsilon=param_list[4])
+                elif self.model == 2:
+                    plot_data['epsilon'] = func_Cole_Cole(freq_list, alpha=param_list[0], tau=param_list[1],
+                                                          epsilon_inf=param_list[2], delta_epsilon=param_list[3])
+                elif self.model == 3:
+                    plot_data['epsilon'] = func_Cole_Davidson(freq_list, beta=param_list[0], tau=param_list[1],
+                                                              epsilon_inf=param_list[2], delta_epsilon=param_list[3])
+                elif self.model == 4:
+                    plot_data['epsilon'] = func_Debye(freq_list, tau=param_list[0], epsilon_inf=param_list[1],
+                                                      delta_epsilon=param_list[2])
+                else:
+                    pass
+        print('PLOT数据', plot_data_dict)
+        # 数据组装获取完成，开始绘图
+        self.fig.clear()
+        ax = self.fig.add_subplot(111)
+        ax.cla()
+        ax.set_xscale('log')
+        for key in plot_data_dict.keys():
+            color_scatter = '#'
+            color_plot = '#'
+            for _ in range(6):
+                random_num = random.randint(0, 7)
+                color_scatter += ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'][
+                    random_num * 2]
+                color_plot += ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'][random_num]
+            print(color_scatter, color_plot)
+            ax.scatter(plot_data_dict[key]['freq'], plot_data_dict[key]['epsilon_raw'], c=color_scatter,
+                       label='#' + key + '_epsilon_raw')
+            ax.plot(plot_data_dict[key]['freq'], plot_data_dict[key]['epsilon'], c=color_plot,
+                    label='#' + key + '_epsilon')
+        ax.legend()
+        self.canvas.draw()
+        self.dialog_plot.open()
+
+    def on_Action_quit(self):
+        sys.exit(0)
+        pass
+
+    def on_Action_save_results_as(self):
+        file_path, file_type = QtWidgets.QFileDialog.getSaveFileName(filter="Text Files (*.txt);;CSV (*.csv)")
+        print(file_path)
+        if file_type == 'Text Files (*.txt)':
+            with open(file_path, 'w') as file:
+                file.write(
+                    "Dielectric Relaxation Simulation Results by Simulator(v1.0alpha), developed by Cheng WANG\n")
+                file.write("Model:\t" + {1: 'Havriliak_Negami Model', 2: 'Cole_Cole Model', 3: 'Cole_Davidson Model',
+                                         4: 'Debye Model'}.get(self.model) + '\n')
+                file.write("#\tAlpha\tBeta\tTau\tEpsilon_Inf\tDelta_Epsilon\n")
+                for key in self.result_dict.keys():
+                    popt = self.result_dict[key]['popt']
+                    print(popt)
+                    file.write(key + '\t')
+                    if self.model == 1:
+                        file.write(str(popt[0]) + '\t' + str(popt[1]) + '\t')
+                    elif self.model == 2:
+                        file.write(str(popt[0]) + '\t1.0\t')
+                    elif self.model == 3:
+                        file.write('1.0\t' + str(popt[0]) + '\t')
+                    elif self.model == 4:
+                        file.write('1.0\t1.0\t')
+                    else:
+                        pass
+                    file.write(str(popt[-3]) + '\t' + str(popt[-2]) + '\t' + str(popt[-1]) + '\n')
+        elif file_type == 'CSV (*.csv)':
+            print(file_path, file_type)
+        else:
+            pass
+
+    def on_Action_parameters_range(self):
+        # TODO:设置参数的上下限
+        pass
 
 
 if __name__ == "__main__":
